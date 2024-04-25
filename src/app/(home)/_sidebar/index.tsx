dea68bb23d7b9e99866f719142c6a7c59316828a
Madhusudhan <madhusudhan1863@gmail.com>
2024-04-25 13:12:14 +0530

    fix: show scroll bar in sidebar only on hover

--- src/app/(home)/_sidebar/index.tsx
+++ src/app/(home)/_sidebar/index.tsx
@@ -1,9 +1,11 @@
-import ConversationsList from "../../../features/conversations/ConversationsList";
-import NewChatButton from "../../../features/conversations/NewChatButton";
+"use client";
+
+import ConversationsList from "@/features/conversations/ConversationsList";
+import NewChatButton from "@/features/conversations/NewChatButton";
 
 const Sidebar = () => {
   return (
-    <div className="w-[260px] h-full bg-[#f9f9f9] overflow-x-hidden flex flex-col">
+    <div className="sidebar w-[260px] h-full bg-[#f9f9f9] overflow-x-hidden flex flex-col transition-all">
       <nav className="h-full w-full px-3 flex-1" aria-label="Chat history">
         <div className="h-full flex-col flex flex-1 transition-opacity duration-500 -mr-2 pr-2 overflow-y-auto">
           <NewChatButton />
