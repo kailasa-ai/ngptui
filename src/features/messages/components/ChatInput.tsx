6fb0e903680bbe642fddb410670de30a7a3ff889
Madhusudhan <madhusudhan1863@gmail.com>
2024-04-29 09:18:05 +0530

    fix: update chat input

--- src/features/messages/components/ChatInput.tsx
+++ src/features/messages/components/ChatInput.tsx
@@ -10,13 +10,15 @@ const ChatInput = (props: Props) => {
   return (
     <textarea
       onKeyDown={props.onKeyDown}
+      rows={1}
+      tabIndex={0}
       className={cn(
         "resize-none border border-gray-400 rounded-lg outline-none h-[52px]",
         "m-0 w-full resize-none border-0 bg-transparent focus:ring-0",
         "focus-visible:ring-0 dark:bg-transparent py-[10px] pr-10",
-        "md:py-3.5 md:pr-12 max-h-[25dvh] max-h-52 placeholder-black/50 dark:placeholder-white/50 pl-4 md:pl-6"
+        "md:py-3.5 md:pr-12 max-h-[25dvh] max-h-52 h-[52px] placeholder-black/50 dark:placeholder-white/50 pl-4 md:pl-6"
       )}
-      placeholder="Chat with Swamiji..."
+      placeholder="Just Ask Nithyananda..."
       onChange={props.onChange}
       value={props.value}
     ></textarea>
