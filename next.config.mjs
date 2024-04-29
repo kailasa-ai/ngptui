e19cf294c120c8e3e880483ffbf53edd6ef11b05
Madhusudhan <madhusudhan1863@gmail.com>
2024-04-29 09:02:27 +0530

    feat: setup progressive web app

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   output: "standalone",
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  reloadOnOnline: true,
+  cacheOnFrontEndNav: false,
+})(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
