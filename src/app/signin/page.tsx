289fa4b97290b367cfd3377a89e8b63518e69c06
Madhusudhan <madhusudhan1863@gmail.com>
2024-05-15 19:29:26 +0530

    fix: redirect to home from signin page if authenticated

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,17 +1,34 @@
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 import { Button } from "@/components/button";
 
-import { signIn } from "@/auth";
+import { auth, signIn } from "@/auth";
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth();
+
+  if (session) {
+    return redirect("/");
+  }
+
   return (
-    <div className="flex items-center justify-center h-full w-full">
+    <div className="h-full w-full flex items-center justify-center">
       <form
         action={async () => {
           "use server";
           await signIn("keycloak", { redirectTo: "/" });
         }}
+        className="-translate-y-[100px] flex flex-col items-center justify-center w-auto h-auto:"
       >
-        <Button>SignIn To Kailasa</Button>
+        <Image src="/ai.png" alt="AI" width={300} height={300} />
+        <Button
+          type="submit"
+          size="lg"
+          className="bg-[#D63C1A] text-white hover:bg-[#BA3C27]"
+        >
+          SignIn To Kailasa
+        </Button>
       </form>
     </div>
   );
