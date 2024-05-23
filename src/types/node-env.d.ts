02a4bd4c22ebab9d1ed1993271c4ab646777f081
Madhusudhan <madhusudhan1863@gmail.com>
2024-05-23 17:26:07 +0530

    fix: update favicon

--- src/types/node-env.d.ts
+++ src/types/node-env.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     APP_BASE_URL: string;
     DIFY_URL: string;
-    DIFY_NITHYNANDAM_API_KEY: string;
+    DIFY_NITHYANANDAM_API_KEY: string;
     DIFY_CHARIYA_PADA_API_KEY: string;
     DIFY_KRIYA_PADA_API_KEY: string;
     DIFY_YOGA_PADA_API_KEY: string;
