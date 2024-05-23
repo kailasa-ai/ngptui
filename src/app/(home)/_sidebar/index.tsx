e43884bda0d8fe7b5193e81433ee2b9cb8221646
Madhusudhan <madhusudhan1863@gmail.com>
2024-05-23 21:06:00 +0530

    fix: remove unwanted styles

--- src/app/(home)/_sidebar/index.tsx
+++ src/app/(home)/_sidebar/index.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar w-full h-full bg-sidebar-surface overflow-x-hidden z-[2000] flex flex-col transition-all">
       <nav className="h-full w-full px-3 flex-1" aria-label="Chat history">
-        <div className="h-full flex-col flex flex-1 transition-opacity duration-500 -mr-2 pr-2 overflow-y-auto grid grid-rows-[auto_1fr_auto]">
+        <div className="h-full transition-opacity duration-500 -mr-2 pr-2 overflow-y-auto grid grid-rows-[auto_1fr_auto]">
           <NewChatButton />
           <ConversationsList />
           <Links />
